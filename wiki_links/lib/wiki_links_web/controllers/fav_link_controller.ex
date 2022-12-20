07a4980a9ef987dea765ec6dd389e0ed43807d12
RamshaKhan978 <gule.ramsha@invozone.com>
2022-12-20 21:26:35 +0500

    issue fixed

--- wiki_links/lib/wiki_links_web/controllers/fav_link_controller.ex
+++ wiki_links/lib/wiki_links_web/controllers/fav_link_controller.ex
@@ -1,7 +1,7 @@
 defmodule WikiLinksWeb.FavLinkController do
   use WikiLinksWeb, :controller
   alias WikiLinks.Wiki_link
-  alias WikiLinks.Wiki_link.Link
+
 
   def index(conn, _params) do
     links = Wiki_link.list_fav_links()
