814f48e4ecdab3c1d84d96dc20b1301a54aee9f1
RamshaKhan978 <gule.ramsha@invozone.com>
2022-12-22 13:22:29 +0500

    user chat updated

--- wiki_links/lib/wiki_links_web/router.ex
+++ wiki_links/lib/wiki_links_web/router.ex
@@ -85,7 +85,7 @@ defmodule WikiLinksWeb.Router do
   scope "/", WikiLinksWeb do
     pipe_through [:browser]
 
-    delete "/users/log_out", UserSessionController, :delete
+    get "/users/log_out", UserSessionController, :delete
     get "/users/confirm", UserConfirmationController, :new
     post "/users/confirm", UserConfirmationController, :create
     get "/users/confirm/:token", UserConfirmationController, :edit
